63eb8780f47f4abb449b448f7cf9c3e044d684f9
sajjad hayati <sajjaddg@gmail.com>
2023-04-08 02:58:59 +0430

    test

--- index.js
+++ index.js
@@ -1,56 +1,16 @@
 import axios from "axios";
+import http from 'http';
 
-const ips = [
-    'discord.com',
-    'test.com'
-]
+const handler = async (req, res) => {
+    const url = req.url
+    const origin = 'https://marzban.bahooacademy.top:8443' + url
+    const data = (await axios.get(origin, {
+        headers: {
+            accept: 'application/json'
+        }
+    })).data
 
-const baseUrl = 'https://marzban.bahooacademy.top:8443'
-const tokenUrl = baseUrl + '/api/admin/token'
-const configUrl = baseUrl + '/api/user/sajjad'
+    res.end(data);
+}
 
-const { access_token } = (await axios.post(tokenUrl, 'username=admin&password=S%40jjaddg', {
-    headers: {
-        "accept": "application/json",
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-})).data
-
-const auth = `Bearer ${access_token}`
-
-const { links: [vmess, vless, trojan] } = (await axios.get(configUrl, {
-    headers: {
-        accept: 'application/json',
-        Authorization: auth
-    }
-})).data
-
-const vmessData = JSON.parse(Buffer.from(vmess.slice(8,), 'base64').toString());
-
-const configs = []
-
-ips.map(item => {
-
-
-    vmessData.add = item
-    const vmessModified = "vmess://" + Buffer.from(JSON.stringify(vmessData)).toString('base64');
-    const vlessModified = vless.replace(/@.*:/, `@${item}:`)
-    const trojanModified = trojan.replace(/@.*:/, `@${item}:`)
-
-    configs.push(vmessModified)
-    configs.push(vlessModified)
-    configs.push(trojanModified)
-})
-
-console.log(configs);
-
-const test = (await axios.get('https://marzban.bahooacademy.top:8443/sub/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhbWluIiwiYWNjZXNzIjoic3Vic2NyaXB0aW9uIiwiaWF0IjoxNjgwOTAzNDUzfQ.UvSWWLgwM4aigZyoyjqApbTacEPokPQmLaolnWhS2Ww', {
-    headers : {
-        accept: 'application/json'
-    }
-})).data
-
-console.log(Buffer.from(test, 'base64').toString())
-
-
-
+http.createServer(handler).listen(3000)
