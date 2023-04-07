29f14b55cf7c4da54f03e3e5164111707f83a823
sajjad hayati <sajjaddg@gmail.com>
2023-04-08 03:27:36 +0430

    test7

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 export default async function handler(req, res) {
   const url = req.url.split('/').slice(2).join('/')
   const origin = 'https://marzban.bahooacademy.top:8443' + url
+  return res.send(origin)
   const data = (await axios.get(origin, {
     headers: {
       accept: 'application/json'
