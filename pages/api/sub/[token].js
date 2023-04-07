604f7d2e135b910c7b53edb5c27274c0eb9ebe59
sajjad hayati <sajjaddg@gmail.com>
2023-04-08 03:34:23 +0430

    test11

--- pages/api/sub/[token].js
+++ pages/api/sub/[token].js
@@ -4,7 +4,6 @@ import axios from 'axios'
 export default async function handler(req, res) {
     const token = req.query.token;
     const origin = 'https://marzban.bahooacademy.top:8443/sub/' + token
-    return res.send(origin)
     const data = (await axios.get(origin, {
         headers: {
             accept: 'application/json'
