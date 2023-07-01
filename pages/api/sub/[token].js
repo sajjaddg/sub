4371c32ec81e40035afbeb4b15a17bb78a531164
sajjad hayati <sajjaddg@gmail.com>
2023-07-01 22:47:11 +0000

    add family

--- pages/api/sub/[token].js
+++ pages/api/sub/[token].js
@@ -1,14 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {useMemo} from 'react'
 import axios from 'axios'
+import data from '../../../data.json'
 
 export default async function handler(req, res) {
     const token = req.query.token;
-    const origin = 'https://host.bahooacademy.top:8443/sub/' + token
-    const data = (await axios.get(origin, {
-        headers: {
-            accept: 'application/json'
-        }
-    })).data
-
-    res.send(data);
+    const mydata = data.join('\r\n')
+    res.send(token==='family'?mydata:null)
 }
